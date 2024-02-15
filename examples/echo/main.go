b5892409a3fae63c473911db4fb1ef602ce8f980
Mohammad Mahdi Afshar <me@mamad.dev>
2024-02-16 02:40:08 +0330

    removed: removed example (temp)

--- examples/echo/main.go
+++ examples/echo/main.go
@@ -1,22 +1,23 @@
 package main
 
-import (
-	tele "go.mamad.dev/telebot"
-	"os"
-)
-
-func main() {
-	tg := tele.New(tele.BotSettings{
-		Token: os.Getenv("TELEGRAM_TOKEN"),
-	})
-
-	tg.Handle("/start", func(c tele.Context) error {
-		return c.Send("Hello Sir, I'm Echo OldBot, Please send me something to echo.")
-	})
-
-	tg.Handle(tele.OnText, func(c tele.Context) error {
-		return c.Send(c.Text())
-	})
-
-	tg.Start()
-}
+//
+//import (
+//	tele "go.mamad.dev/telebot"
+//	"os"
+//)
+//
+//func main() {
+//	tg := tele.New(tele.BotSettings{
+//		Token: os.Getenv("TELEGRAM_TOKEN"),
+//	})
+//
+//	tg.Handle("/start", func(c tele.Context) error {
+//		return c.Send("Hello Sir, I'm Echo OldBot, Please send me something to echo.")
+//	})
+//
+//	tg.Handle(tele.OnText, func(c tele.Context) error {
+//		return c.Send(c.Text())
+//	})
+//
+//	tg.Start()
+//}
