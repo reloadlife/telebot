e09266019a929ae0390bb9f9c4975f0278c7fc70
Mohammad Mahdi Afshar <me@mamad.dev>
2024-02-19 19:02:53 +0330

    test: sendMessage test

--- message_test.go
+++ message_test.go
@@ -2,8 +2,34 @@ package telebot
 
 import (
 	"github.com/stretchr/testify/require"
+	"os"
+	"strconv"
 	"testing"
 )
+
+func Test_Online_SendMessage(t *testing.T) {
+	tg := GetBot()
+
+	chatID, hasChatID := os.LookupEnv("CHAT_ID")
+
+	if !hasChatID {
+		panic("CHAT_ID is not set")
+	}
+
+	chat, err := strconv.ParseInt(chatID, 10, 64)
+
+	require.NoError(t, err)
+
+	whereTo := &Chat{ID: chat}
+
+	b, err := tg.SendMessage(whereTo, "Hello, World!")
+	require.NoError(t, err)
+
+	require.NotNil(t, b)
+
+	require.Equal(t, b.Chat.ID, chat)
+	require.Equal(t, b.Text, "Hello, World!")
+}
 
 func TestMessage(t *testing.T) {
 	t.Run("MaybeInaccessibleMessage", func(t *testing.T) {
@@ -45,5 +71,4 @@ func TestMessage(t *testing.T) {
 
 	iMsg := &InaccessibleMessage{}
 	require.NotNil(t, iMsg)
-
 }
